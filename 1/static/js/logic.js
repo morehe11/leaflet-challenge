9559b103e434e5ae7a00b3d9d06f7d4ded8c9239
Jenna_Morehead <morehe11@msu.edu>
2021-08-20 23:15:44 -0400

    final push

--- 1/static/js/logic.js
+++ 1/static/js/logic.js
@@ -24,61 +24,65 @@ graymap.addTo(myMap)
 
 var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 d3.json(baseURL).then(function(earthquakeData, err) {
-    L.geoJson(earthquakeData,{
-        pointToLayer: function(features, latlng){
-            return L.circleMarker(latlng, {
-                color: "red", 
-                fillColor: getColor(latlng.alt),
-                stroke: true, 
-                
-            }).bindPopup(function(earthquakeData){
-                return `Location: ${features.properties.place}<br>Magnitude: ${features.properties.mag}`;
-            })
-        }
-    }).addTo(myMap);
+    function markersize(magnitude){
+        return magnitude *4;
+    };
+   
     function getColor(magnitude){
         switch (true){
-            case magnitude > 5:
-                return "red";;
-            case magnitude > 4:
+            case magnitude > 90:
+                return "red";
+            case magnitude > 70:
                 return "redOrange";
-            case magnitude > 3:
+            case magnitude > 50:
                 return "orange";
-            case magnitude > 2:
+            case magnitude > 30:
                 return "gold";
-            case magnitude > 1:
+            case magnitude > 10:
                 return "yellow";
             default:
                 return "blue";
             
         }
     }
-});
+    
 
+L.geoJSON(earthquakeData,{
+    pointToLayer: function(feature, latlng){
+        return L.circleMarker(latlng,{
+            radius: markersize(feature.properties.mag),
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            fillOpacity: 0.5,
+            color:"red",
+            stroke: true,
+        }
+        );
+    },
+    
+    onEachFeature: function(feature,layer){
+        layer.bindPopup(`Location: ${feature.properties.place}<br>Magnitude: ${feature.properties.mag}`);
+
+    },
+
+}).addTo(myMap);
+
+var legend = L.control({position: "bottom right"});
+legend.onAdd = function(){
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [5,4,3,2,1,0]
+    div.innerHTML += "<h3 style = 'text-align: center'>Depth</h3>"
+for (var i =0; i<depth.length; i++){
+    div.innerHTML += 
+    '<i style = background:' + getColor(depth[i]+1) + "></i" +
+    depth[i] + (depth[i+1]? '&ndash;' + depth[i+1]+ '<br>': '+');
+}
+return div;
+};
+legend.addTo(myMap)});
 var legend = L.control({
     position: "bottom right"
 });
 
-legend.onAdd = function(){
-    var div = L.DomUtil.create("div", "legend");
-
-    var grades = [5,4,3,2,1,0];
-    var colors = [
-        "red",
-        "redorange",
-        "orange",
-        "gold",
-        "yellow",
-        "blue"
-    ];
-    for (var i = 0; i<grades.length; i++){
-        div.innerHTML += "<i style = 'backgroud : " + colors[i] + "></i"
-        + grades[i] + (grades[i +1]? "&ndash;" + grades[i+1]+ "<br>": "+")
-    }
-    return div;
-};
-
-legend.addTo(myMap);
 
 
 
